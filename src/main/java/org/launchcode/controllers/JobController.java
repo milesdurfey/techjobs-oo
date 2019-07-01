68591fe88f4ecb4ec3854c5e633a0569b578208e
Miles Durfey <mdurfey@gmail.com>
2019-07-01 18:47:40 -0500

    Commit for github - TODOs completed

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,7 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.*;
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.text.AttributedString;
 
 /**
  * Created by LaunchCode
@@ -24,6 +27,8 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
+        Job jobWithId = jobData.findById(id);
+        model.addAttribute("job", jobWithId);
 
         return "job-detail";
     }
@@ -41,7 +46,22 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "";
+        if (errors.hasErrors()) {
+            return "new-job";
+        }
+
+        Job newJob = new Job();
+
+        newJob.setName(jobForm.getName());
+        newJob.setEmployer(jobData.getEmployers().findById(jobForm.getEmployerId()));
+        newJob.setLocation(jobData.getLocations().findById(jobForm.getLocationId()));
+        newJob.setCoreCompetency(jobData.getCoreCompetencies().findById(jobForm.getCoreCompetenciesId()));
+        newJob.setPositionType(jobData.getPositionTypes().findById(jobForm.getPositionTypeId()));
+
+        jobData.add(newJob);
+
+//        return "redirect:?id=" + newJob.getId();
+        return "redirect:/job?id=" + newJob.getId();
 
     }
 }
